ffe0a40fdb1455706a51e2871f9c6a0ddda6d1c0
hjk216 <62774444+hjk216@users.noreply.github.com>
2021-08-03 17:50:18 -0400

    merge sort

--- src/features/algorithms/mergeSort.js
+++ src/features/algorithms/mergeSort.js
@@ -1,56 +1,81 @@
 export default async function MergeSort(array, speed) {
     //const bars = document.getElementsByClassName('bar');
 
-    array = [0,2,4,6,8,1,3,5,7,9]
+    array = [2,0,4,1,3,9,8,5]
 
     console.log(array);
 
+    // Sort Array
     async function sortArray(array) {
-        var left = array.slice(0,array.length/2);
+        var left = array.slice(0, array.length/2);
         var right = array.slice(array.length/2, array.length);
-        var sorted = [];
+        var sorted_array = [];
 
         var l = 0;
         var r = 0;
         while(true) {
             if(left[l] < right[r]) {
-                sorted.push(left[l]);
+                sorted_array.push(left[l]);
                 l = l + 1;
             } else {
-                sorted.push(right[r]);
+                sorted_array.push(right[r]);
                 r = r + 1;
             }
 
             if(l === left.length) {
                 for(var i = r; i < right.length; i++) {
-                    sorted.push(right[i])
+                    sorted_array.push(right[i]);
                 }
                 break;
             }
             if(r === right.length) {
                 for(var j = l; j < left.length; j++) {
-                    sorted.push(left[j])
+                    sorted_array.push(left[j]);
                 }
                 break;
             }
 
         }
-        array = sorted;
+        return sorted_array;
     }
-    sortArray(array);
 
-    
-
-    
-
+    //function mergeSort() {
+        
+    //}
 
 
+    var num = 0;
+    var right = 0;
+    var left = 4;
+    while(num < array.length - 1) {
+        var current_array = array.slice(right, left);
+        let starting_index = right;
+        console.log(current_array)
+        sortArray(array).then((sorted_array) => {
+            for(var n = 0; n < sorted_array.length; n++) {
+                array[starting_index] = sorted_array[n];
+                starting_index = starting_index + 1;
+            }
+            console.log(array);
+        })
 
-    console.log(array);
+        num = num + 1;
+        return
+    }
+
 }
+
+
+// [ 2 , 0 , 4 , 1 , 3 , 9 , 8 , 5 ]
+
+// [ 2 , 0 , 4 , 1 ]   [ 3 , 9 , 8 , 5 ]
+
+// [ 2 , 0 ] [ 4 , 1 ]   [ 3 , 9 ] [ 8 , 5 ]
+
+// 
 
 
 // [ 0 2 4 1 ]
 // [ 0 2 ]
 // [ 1 4 ]
-// [ 0 1 2 4]
+// [ 0 1 2 4 ]

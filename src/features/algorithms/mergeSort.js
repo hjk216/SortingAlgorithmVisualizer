80e1e69d06f0519cf2e71419ba52e26ed2e5eda4
hjk216 <62774444+hjk216@users.noreply.github.com>
2021-08-04 21:50:13 -0400

    merge sort algorithm

--- src/features/algorithms/mergeSort.js
+++ src/features/algorithms/mergeSort.js
@@ -1,81 +1,144 @@
 export default async function MergeSort(array, speed) {
     //const bars = document.getElementsByClassName('bar');
 
-    array = [2,0,4,1,3,9,8,5]
+    //array = [6,7,8,9,1,2,3,4] // Testing
+    array = [8,2,5,3,7,9,1,6,3,3]
 
-    console.log(array);
+    console.log(array); // Testing
 
-    // Sort Array
-    async function sortArray(array) {
-        var left = array.slice(0, array.length/2);
-        var right = array.slice(array.length/2, array.length);
-        var sorted_array = [];
+    merge_sort(array, 0, array.length - 1);
+    //var q = Math.floor((0 + array.length) / 2); // Testing
+    //merge(array, 0, q, array.length - 1) // Testing
+    console.log(array); // Testing
 
-        var l = 0;
-        var r = 0;
-        while(true) {
-            if(left[l] < right[r]) {
-                sorted_array.push(left[l]);
-                l = l + 1;
-            } else {
-                sorted_array.push(right[r]);
-                r = r + 1;
-            }
-
-            if(l === left.length) {
-                for(var i = r; i < right.length; i++) {
-                    sorted_array.push(right[i]);
-                }
-                break;
-            }
-            if(r === right.length) {
-                for(var j = l; j < left.length; j++) {
-                    sorted_array.push(left[j]);
-                }
-                break;
-            }
-
-        }
-        return sorted_array;
-    }
-
-    //function mergeSort() {
-        
-    //}
-
-
-    var num = 0;
-    var right = 0;
-    var left = 4;
-    while(num < array.length - 1) {
-        var current_array = array.slice(right, left);
-        let starting_index = right;
-        console.log(current_array)
-        sortArray(array).then((sorted_array) => {
-            for(var n = 0; n < sorted_array.length; n++) {
-                array[starting_index] = sorted_array[n];
-                starting_index = starting_index + 1;
-            }
-            console.log(array);
-        })
-
-        num = num + 1;
-        return
-    }
-
+    return array;
 }
 
 
-// [ 2 , 0 , 4 , 1 , 3 , 9 , 8 , 5 ]
 
-// [ 2 , 0 , 4 , 1 ]   [ 3 , 9 , 8 , 5 ]
+function merge_sort(array, p, r) {
+    if(p < r) {
+        var q = Math.floor((p + r) / 2);
 
-// [ 2 , 0 ] [ 4 , 1 ]   [ 3 , 9 ] [ 8 , 5 ]
+        merge_sort(array, p, q);
+        merge_sort(array, q + 1, r);
+        merge(array, p, q, r);
+    }
+}
 
-// 
 
 
-// [ 0 2 4 1 ]
-// [ 0 2 ]
-// [ 1 4 ]
-// [ 0 1 2 4 ]
+function merge(array, p, q, r) {
+    var n1 = q - p + 1;
+    var n2 = r - q + 1;
+    
+    //console.log('   N1: ' + n1); // Testing
+    //console.log('   N2: ' + n2); // Testing
+    //console.log(' '); // Testing
+
+    var left = [];
+    var right = [];
+
+    for(var i = 0; i < n1; i++) {
+        left.push(array[p + i]);
+    }
+    for(var j = 1; j < n2; j++) {
+        right.push(array[q + j]);
+    }
+
+    //console.log('LEFT : ' +  left); // Testing
+    //console.log('RIGHT: ' + right); // Testing
+    //console.log(' '); // Testing
+
+    i = 0;
+    j = 0;
+
+    for(var k = p; k < r + 1; k++) {
+        if(i > (left.length - 1)) {
+            array[k] = right[j];
+            j = j + 1;
+            continue;
+        }
+        if(j > (right.length - 1)) {
+            array[k] = left[i];
+            i = i + 1;
+            continue;
+        }
+
+        if(left[i] <= right[j]) {
+            array[k] = left[i];
+            i = i + 1;
+        }
+        else {
+            array[k] = right[j];
+            j = j + 1;
+        }
+    }
+    //console.log('ARRAY: ' + array); // Testing
+}
+
+
+
+
+/* 
+function merge_sort(array, p, r) {
+    if(p < r) {
+        var q = Math.floor((p + r) / 2);
+
+        merge_sort(array, p, q);
+        merge_sort(array, q + 1, r);
+        merge(array, p, q, r);
+    }
+}
+
+
+
+function merge(array, p, q, r) {
+    var n1 = q - p + 1;
+    var n2 = r - q + 1;
+    
+    //console.log('   N1: ' + n1); // Testing
+    //console.log('   N2: ' + n2); // Testing
+    //console.log(' '); // Testing
+
+    var left = [];
+    var right = [];
+
+    for(var i = 0; i < n1; i++) {
+        left.push(array[p + i]);
+    }
+    for(var j = 1; j < n2; j++) {
+        right.push(array[q + j]);
+    }
+
+    //console.log('LEFT : ' +  left); // Testing
+    //console.log('RIGHT: ' + right); // Testing
+    //console.log(' '); // Testing
+
+    i = 0;
+    j = 0;
+
+    for(var k = p; k < r + 1; k++) {
+        if(i > (left.length - 1)) {
+            array[k] = right[j];
+            j = j + 1;
+            continue;
+        }
+        if(j > (right.length - 1)) {
+            array[k] = left[i];
+            i = i + 1;
+            continue;
+        }
+
+        if(left[i] <= right[j]) {
+            array[k] = left[i];
+            i = i + 1;
+        }
+        else {
+            array[k] = right[j];
+            j = j + 1;
+        }
+    }
+    //console.log('ARRAY: ' + array); // Testing
+}
+*/
